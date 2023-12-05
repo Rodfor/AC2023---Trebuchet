418fc68a36a652f17fe6f3a972e8a7bfecffa460
Matthias <matthias.leys@belorta.be>
2023-12-05 15:00:11 +0100

    Add project files.

---
+++ 1 - trebuchet.vb
@@ -0,0 +1,53 @@
+﻿Imports System.IO
+Imports Microsoft.VisualBasic.FileIO
+Imports System.Text.RegularExpressions
+Module Module1
+
+    Sub Main()
+        Dim pad = "C:\Users\mle.SERVER\Downloads\trebuchet.txt"
+        Dim lijnen As New List(Of String())
+
+        Using parser = New TextFieldParser(pad)
+            parser.SetDelimiters(" ")
+            While Not parser.EndOfData
+                Dim fields As String() = parser.ReadFields()
+                lijnen.Add(fields)
+            End While
+
+            parser.Close()
+            parser.Dispose()
+        End Using
+
+        Dim Nummer As Integer = 0
+        Dim nummers As New List(Of String)
+
+        For Each lijn In lijnen
+            Dim tekst As String = lijn(0)
+
+            tekst = tekst.Replace("zero", "z0ro")
+            tekst = tekst.Replace("one", "o1e")
+            tekst = tekst.Replace("two", "t2o")
+            tekst = tekst.Replace("three", "t3ree")
+            tekst = tekst.Replace("four", "f4ur")
+            tekst = tekst.Replace("five", "f5ve")
+            tekst = tekst.Replace("six", "s6x")
+            tekst = tekst.Replace("seven", "s7ven")
+            tekst = tekst.Replace("eight", "e8ght")
+            tekst = tekst.Replace("nine", "n9ne")
+
+            Console.WriteLine(tekst)
+            Dim num As String = Regex.Replace(tekst, "[^0-9]", String.Empty)
+
+            Dim firstdigit As String = num.First
+            Dim lastdigit As String = ""
+            lastdigit = num.Last
+            Dim newnum As String = firstdigit + lastdigit
+            Nummer += newnum
+        Next
+
+        Console.WriteLine(Nummer)
+
+        Dim x = Console.ReadLine()
+    End Sub
+
+End Module
---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("AC2023 - Trebuchet")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("HP")>
+<Assembly: AssemblyProduct("AC2023 - Trebuchet")>
+<Assembly: AssemblyCopyright("Copyright © HP 2023")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("dee11359-afc8-4d4f-87fd-d7c03e0f06f9")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("2023.12.05.1449")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
