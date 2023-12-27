b800d17a501f09a26a21b4d3f391a2bce54d065f
Matthias <matthias.leys@belorta.be>
2023-12-27 17:00:06 +0100

    21 deel p2

--- 21 - Steps.vb
+++ 21 - Steps.vb
@@ -1,81 +1,108 @@
 ﻿Module Module21
+    Dim Pattern(,) As Node
+
     Public Sub Steps()
         Dim pad = "C:\Users\mle.SERVER\source\repos\AC2023 - Trebuchet\Steps.txt"
         Dim Lijnen() As String = System.IO.File.ReadAllLines(pad)
 
-        Dim Pattern(Lijnen.Count - 1, Lijnen.First.Length - 1) As Node
-        Dim Possibilities As New List(Of Node)
+        ReDim Pattern(Lijnen.Count - 1, Lijnen.First.Length - 1)
+        Dim startnode As Node
+        Dim index As Integer
 
         For i = 0 To Lijnen.Count() - 1
             Dim lijn = Lijnen(i)
             For j = 0 To lijn.Length - 1
                 Pattern(i, j) = New Node(i, j, lijn(j))
                 If lijn(j) = "S" Then
-                    Possibilities.Add(Pattern(i, j))
-                    Possibilities.First.value = "."
+                    Pattern(i, j).value = "."
+                    startnode = Pattern(i, j)
+                    index = j
                 End If
             Next
         Next
 
-        Dim steps As Integer = 70
-        Dim count As Integer = 0
 
-        Dim FastUpFirst As Path = Nothing
-        Dim FastDownFirst As Path = Nothing
-        Dim FastLeftFirst As Path = Nothing
-        Dim FastRightFirst As Path = Nothing
+
+        Dim D = 26501365
+        Dim w = Pattern.GetUpperBound(0) + 1
+        Dim N = (D - index) / w
+
+        Dim E = Flood(startnode, 3 * w)
+        Dim O = Flood(startnode, (3 * w) + 1)
+
+        Dim sA = ((3 * w) - 3) / 2
+        Dim A = Flood(Pattern(0, 0), sA) + Flood(Pattern(0, Pattern.GetUpperBound(1)), sA) + Flood(Pattern(Pattern.GetUpperBound(0), 0), sA) + Flood(Pattern(Pattern.GetUpperBound(0), Pattern.GetUpperBound(1)), sA)
+        Dim sB = (w - 3) / 2
+        Dim B = Flood(Pattern(0, 0), sB) + Flood(Pattern(0, Pattern.GetUpperBound(1)), sB) + Flood(Pattern(Pattern.GetUpperBound(0), 0), sB) + Flood(Pattern(Pattern.GetUpperBound(0), Pattern.GetUpperBound(1)), sB)
+        Dim T = Flood(Pattern(0, index), w) + Flood(Pattern(index, index), w) + Flood(Pattern(w - 1, index), w) + Flood(Pattern(index, w - 1), w)
+
+
+
+        Dim uitkomtst = ((N - 1) ^ 2) * O + (N ^ 2) * E + (N - 1) * A + N * B + T
+
+        Console.WriteLine(uitkomtst)
+
+
+    End Sub
+
+    Public Function Flood(start As Node, steps As Integer) As Integer
+        Dim count = 0
+        Dim possibilities As New List(Of Node)
+        possibilities.Add(start)
+        start.flooded = True
 
         While count < steps
             count += 1
             Dim newPosibilities As New List(Of Node)
             For Each N In Possibilities
                 If N.x > Pattern.GetLowerBound(0) AndAlso Pattern(N.x - 1, N.y).value = "." Then
-                    If Not newPosibilities.Contains(Pattern(N.x - 1, N.y)) Then
+                    If Not Pattern(N.x - 1, N.y).flooded Then
                         newPosibilities.Add(Pattern(N.x - 1, N.y))
-                        If N.x = Pattern.GetLowerBound(0) + 1 AndAlso FastUpFirst Is Nothing Then FastUpFirst = New Path(Pattern(N.x - 1, N.y), count)
+
                     End If
                 End If
                 If N.x < Pattern.GetUpperBound(0) AndAlso Pattern(N.x + 1, N.y).value = "." Then
-                    If Not newPosibilities.Contains(Pattern(N.x + 1, N.y)) Then
+                    If Not Pattern(N.x + 1, N.y).flooded Then
                         newPosibilities.Add(Pattern(N.x + 1, N.y))
-                        If N.x = Pattern.GetUpperBound(0) - 1 AndAlso FastDownFirst Is Nothing Then FastDownFirst = New Path(Pattern(N.x + 1, N.y), count)
+
                     End If
                 End If
                 If N.y > Pattern.GetLowerBound(1) AndAlso Pattern(N.x, N.y - 1).value = "." Then
-                    If Not newPosibilities.Contains(Pattern(N.x, N.y - 1)) Then
+                    If Not Pattern(N.x, N.y - 1).flooded Then
                         newPosibilities.Add(Pattern(N.x, N.y - 1))
-                        If N.y = Pattern.GetLowerBound(1) + 1 AndAlso FastLeftFirst Is Nothing Then FastLeftFirst = New Path(Pattern(N.x, N.y - 1), count)
+
                     End If
                 End If
                 If N.y < Pattern.GetUpperBound(1) AndAlso Pattern(N.x, N.y + 1).value = "." Then
-                    If Not newPosibilities.Contains(Pattern(N.x, N.y + 1)) Then
+                    If Not Pattern(N.x, N.y + 1).flooded Then
                         newPosibilities.Add(Pattern(N.x, N.y + 1))
-                        If N.y = Pattern.GetUpperBound(1) - 1 AndAlso FastRightFirst Is Nothing Then FastRightFirst = New Path(Pattern(N.x, N.y + 1), count)
+
                     End If
                 End If
             Next
 
             Possibilities = newPosibilities
-            Console.WriteLine(Possibilities.Count)
         End While
 
-        Console.WriteLine(Possibilities.Count)
+        Dim flooded As Integer = 0
 
-        For i = 0 To Pattern.GetUpperBound(0)
-            For j = 0 To Pattern.GetUpperBound(1)
-                If Pattern(i, j).value = "." AndAlso Not Possibilities.Contains(Pattern(i, j)) And (i + j) Mod 2 = 1 Then
-                    Console.WriteLine(i.ToString + "," + j.ToString + " unreachable")
-                End If
-            Next
+        For Each N In Pattern
+            If N.flooded Then
+                flooded += 1
+                N.flooded = False
+            End If
         Next
 
-    End Sub
+        Return flooded
+
+    End Function
 
 
     Public Class Node
         Public x As Integer
         Public y As Integer
         Public value As Char
+        Public flooded As Boolean = False
 
         Public Sub New(x As Integer, y As Integer, value As Char)
             Me.x = x
--- My Project/AssemblyInfo.vb
+++ My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")>
 
-<Assembly: AssemblyVersion("2023.12.26.1555")>
+<Assembly: AssemblyVersion("2023.12.27.1656")>
 <Assembly: AssemblyFileVersion("1.0.0.0")>
