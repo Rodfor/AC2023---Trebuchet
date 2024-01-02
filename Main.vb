74cac290a4b2542512324a31ffb184edf83f251c
Matthias <matthias.leys@belorta.be>
2024-01-02 15:41:38 +0100

    23 - part 1

--- Main.vb
+++ Main.vb
@@ -1,6 +1,6 @@
 ﻿Module Main
     Sub Main()
-        Bricks()
+        Walk()
 
         Dim x = Console.ReadLine()
     End Sub
--- My Project/AssemblyInfo.vb
+++ My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")>
 
-<Assembly: AssemblyVersion("2023.12.29.1238")>
+<Assembly: AssemblyVersion("2024.01.02.1440")>
 <Assembly: AssemblyFileVersion("1.0.0.0")>
